d40fc65e392c71f824282253bd0dc0371de41ebd
pitburak@gmail.com <pit@MSI.localdomain>
2024-01-15 14:03:43 +0000

    Task 2 done

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//
+const express = require('express');
+const app = express();
+const { getTopics } = require('./controlers/controlers');
+
+app.use(express.json());
+
+app.get('/api/topics', getTopics);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next(err);
+});
+
+module.exports = app;
